714ff49fff0bf5c7803337871c94299be30dabf1
BenardNjenga <njengab203@gmail.com>
2026-09-14 23:16:18 +0300

    Introduction to C programming _S1Y1 units. c
    C programming assignment for S1Y1 units

---
+++ Introduction to C programming _ S1Y1 units.c
@@ -0,0 +1,18 @@
+/*
+Name:Benard Karomo
+Reg no: CT100/G/30708/26
+Description:output of semetser 1 year 1 units 
+*/
+
+#include  <stdio.h>
+
+ int main(){
+       printf("Fundamentals of Digital Technology and Artificial Inteligence.\n");
+       printf("Introduction to computer application.\n");
+       printf("Analogue electronics.\n");
+       printf("Mathematics of Science.\n");
+       printf("Introduction to programming and Algorithms.\n");
+       printf("Communication and Information Literacy skills.\n");
+
+    return 0;
+}
